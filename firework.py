991e857480868b57e463092940472a20107d7a66
Sean O'Connor <soc1641@icloud.com>
2026-04-20 12:29:34 -0500

    added explode function to firework

--- firework.py
+++ firework.py
@@ -1,6 +1,8 @@
 import numpy as np
+import math
 import pygame
 import random
+import particle
 
 class firework:
     def __init__(self, mass, pos, velocity, target, color, colors, timer):
@@ -21,8 +23,17 @@ class firework:
 
     def explode(self, list):
         # spawn the light particles
+        for _ in range(200):
+            angle = random.uniform(0, 2 * math.pi)
+            speed = random.uniform(5, 30)
+            vx = math.cos(angle)
+            vy = math.sin(angle)
+            velocity = np.array([vx, vy]) * speed
 
-        pass
+            list.append(particle.particle(1, self.pos, velocity, self.colors[int(random.random() * len(self.colors))], 5))
+
+
+        
 
     def draw(self, surface):
-        pygame.draw.circle(surface, self.color, tuple(self.pos), 10, width=0)
+        pygame.draw.circle(surface, self.color, tuple(self.pos), 5, width=0)
